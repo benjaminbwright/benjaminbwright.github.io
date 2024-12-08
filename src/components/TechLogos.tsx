50d9142141aa81972b8d9153d8f09f0b0b80d9a8
Benjamin Wright <benjamin.b.wright@gmail.com>
2024-12-07 23:44:28 -0600

    adds javascript log

---
+++ src/components/TechLogos.tsx
@@ -0,0 +1,11 @@
+import jsLogo from "../assets/java-script.png";
+
+const TechLogos = () => {
+  return (
+    <div className="flex">
+      <img src={jsLogo} alt="javascript" />
+    </div>
+  );
+};
+
+export default TechLogos;
