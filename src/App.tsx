67592c05c84ecabfa9b0a9d7dad3f7a1303f16d9
Benjamin Wright <benjamin.b.wright@gmail.com>
2024-12-07 23:47:47 -0600

    header file capitalization issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Header from "./components/header";
+import Header from "./components/Header";
 import HeadshotContactCard from "./components/HeadshotContactCard";
 import "./App.css";
 import TechLogos from "./components/TechLogos";
