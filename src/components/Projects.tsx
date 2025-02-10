51cba65280580da970056fbf22a7f119cb491125
Benjamin Wright <benjamin.b.wright@gmail.com>
2025-02-10 11:39:01 -0600

    adds padding and border around the section

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,6 @@
 function Projects() {
   return (
-    <section>
+    <section className="w-full border-t-2 border-black mt-4 pt-5">
       <h2 className="font-roboto font-bold uppercase text-4xl">Work</h2>
       <p>
         I've worked with a number of companies and organizations to build
